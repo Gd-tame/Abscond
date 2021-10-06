35089e7b4f429320c109d61060278ae579b88d29
Aryan <aryan2563737@gmail.com>
2021-10-07 00:18:50 +0530

    Initializing Repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+import Login from "./Components/Login"
 
+//? Main App Component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledApp>
+      <Login />
+    </StyledApp>
+  )
 }
 
-export default App;
+const StyledApp = styled.div`
+  width: 100%;
+  background: linear-gradient(to bottom, #9c99ef, #fac7d5);
+`
+
+export default App
---
+++ src/Components/Login/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+function Login() {
+  return <StyledLogin>Login</StyledLogin>
+}
+
+const StyledLogin = styled.main`
+  width: 100%;
+  height: 400vh;
+`
+export default Login
